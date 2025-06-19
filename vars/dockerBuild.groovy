1c6a4bd7d04e85da5060df4e1a4334c06673b5c7
Sairamguthula8 <Sairamguthula8@gmail.com>
2025-06-19 19:24:52 +0530

    jnno

---
+++ vars/dockerBuild.groovy
@@ -0,0 +1,9 @@
+dockerHubUsername, String imageName) {
+    // Build the Docker image
+    sh "docker build --build-arg REACT_APP_RAPID_API_KEY=6bb3817f24mshabfe50278881b03p15eaedjsn16fa8b96225e -t ${imageName} ."
+     // Tag the Docker image
+    sh "docker tag ${imageName} ${dockerHubUsername}/${imageName}:latest"
+    // Push the Docker image
+    withDockerRegistry([url: 'https://index.docker.io/v1/', credentialsId: 'docker']) {
+        sh "docker push ${dockerHubUsername}/${imageName}:latest"
+    }
